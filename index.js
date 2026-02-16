fe94ac37662c58895a999c307a8879470f2efa2b
ABDIRIZACK <abdirizack466@gmail.com>
2026-02-16 08:49:56 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+  const button = document.getElementById('button');
+
+  button.addEventListener('click', function() {
+    console.log('Button clicked');
+  });
+}
+
